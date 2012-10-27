2a5105eaa5bf3cb4c6a8b0ed1ab06adcfb6070e7
na1s <nasyrov@gmail.com>
2012-10-28 00:58:22 +0500

    added fake repository

--- tests/fake_repository.py
+++ tests/fake_repository.py
@@ -1,18 +1,12 @@
 __author__ = 'denis'
-
+from mercurial import patch, util
 class FakeRepository:
 
     def create(self, ui, reponame):
-        return mercurial.hg.repository(ui, reponame, create = 1)
+        None
 
-    def add(self, ui, repo, filename):
-        mercurial.commands.add(ui, repo, filename)
+    def add_file(self, ui, repo, filename):
+        None
 
-    def commit(self, ui, repo, filename, message = '', user = '', date = ''):
-        if not message:
-            message = 'Added %s.' % filename
-        if not user:
-            user = 'jdoe'
-        if not date:
-            date = str(int(time.time())) + ' 0'
-        mercurial.commands.commit(ui, repo, filename, message = message, user = user, date = date)
+    def commit_file(self, ui, repo, filename, message = '', user = '', date = ''):
+        None
