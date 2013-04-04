098f4fb5e4584715c4830722f3a5a55f42bf927c
na1s <nasyrovd@gmail.com>
2013-04-04 13:04:44 +0600

    reformat

--- __init__.py
+++ __init__.py
@@ -1,13 +1,14 @@
 def generate_statistic(ui, repo, **opts):
     ui.write('Common repository statistics\r\n')
-    ui.write("Heads count:%d\r\n"%len(repo.heads()))
-    ui.write("Heads count:%d\r\n"%len(repo.branchtags()))
+    ui.write("Heads count:%d\r\n" % len(repo.heads()))
+    ui.write("Heads count:%d\r\n" % len(repo.branchtags()))
+
 
 cmdtable = {
     # cmd name        function call
     'hgstat': (generate_statistic,
                # See mercurial/fancyopts.py for all of the command flag options.
-               [('u', 'user', "", 'print statistics per user'),],
+               [('u', 'user', "", 'print statistics per user'), ],
                '[options] REV')
 }
 
