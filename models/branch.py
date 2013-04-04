8b59b4fba8b023924c98e2e9f03963e019c5c5c2
na1s <nasyrovd@gmail.com>
2013-04-04 17:39:56 +0600

    reformat

--- models/branch.py
+++ models/branch.py
@@ -4,11 +4,12 @@ from os import path
 from datetime import datetime
 from mercurial import patch, util
 from mercurial.templatefilters import person
+
 __author__ = 'denis'
 
 
 class Branch:
-    def __init__(self,repository, name, node, status):
+    def __init__(self, repository, name, node, status):
         self.status = status
         self.name = name
         self.node = node
@@ -16,16 +17,20 @@ class Branch:
 
     def status(self):
         return self.status
+
     def get_name(self):
         return self.name
+
     def get_node(self):
         return self.node
+
     def get_date(self):
         mercurial_date = self.repository.repo[self.get_node()].date()[0]
         date = gmtime(mercurial_date)
         return date
+
     def get_user(self):
         node_user = self.repository.repo[self.get_node()].user()
         email = util.email(node_user)
         name = person(node_user)
-        return name,email
+        return name, email
