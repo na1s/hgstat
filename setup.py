e05778bd018e120b0a4674d169c5600d1d50a953
na1s <nasyrovd@gmail.com>
2013-04-17 09:36:57 +0600

    added setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='hgstat',
+    version='0.1',
+    description='Hg statistical extension',
+    author='Denis Nasyrov',
+    author_email='denis.nasyrov@yandex.ru',
+    url='http://github.com/na1s/hgstat/',
+    py_modules=['hgstat/sample'],
+    license='GNU General Public License (GPL) 2.0 or later'
+)
